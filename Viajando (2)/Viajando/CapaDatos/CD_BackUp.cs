15b2dbf36d986e55f9c82357cfb1388343fa5f73
Claudio Castellano <castellanoporto@gmail.com>
2025-05-02 14:45:49 -0300

    Nuevos cambios

---
+++ Viajando (2)/Viajando/CapaDatos/CD_BackUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class CD_BackUp : Conexion
+    {
+
+        SqlCommand cmd = new SqlCommand();
+        Conexion conexion = new Conexion();
+
+        public void RealizarBackup(string rutaDestino)
+        {
+            try
+            {
+                conexion.AbrirConexion();
+                cmd.CommandText = "RealizarBackUp";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Ruta", rutaDestino);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al ejecutar SP o Conexion a la BD. \n \n" + ex.Message);
+            }
+            finally
+            {
+                cmd.Parameters.Clear();
+                conexion.CerrarConexion();
+            }
+        }
+    }
+
+}
+
