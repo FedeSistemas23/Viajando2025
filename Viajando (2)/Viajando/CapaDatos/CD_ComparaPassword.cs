055b85554bd21a9af94b205180ce1c7e8de81850
Fede <fededelveliz2017@gmail.com>
2025-05-19 21:10:07 -0300

    .

---
+++ Viajando (2)/Viajando/CapaDatos/CD_ComparaPassword.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_ComparaPassword
+    {
+        public bool ComparaPassD(string username, string passwordHasheado)
+        {
+            try
+            {
+                using (SqlConnection connection = new Conexion().AbrirConexion())
+                using (SqlCommand cmd = new SqlCommand("VerificarPassword", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Pass", passwordHasheado);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int resultado = reader.GetInt32(reader.GetOrdinal("Resultado"));
+                            return resultado == 1;
+                        }
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al ejecutar el procedimiento almacenado o al conectar con la base de datos.\n\n" + ex.Message);
+            }
+        }
+    }
+}
---
+++ Viajando (2)/Viajando/CapaNegocio/CN_ComparaPassword.cs
@@ -0,0 +1,31 @@
+﻿using CapaDatos;
+using CapaSesion;
+using Microsoft.SqlServer.Server;
+
+namespace CapaNegocio
+ 
+{
+    public class CN_ComparaPassword
+    {
+        CD_ComparaPassword comparador;
+
+        public CN_ComparaPassword()
+        {
+            comparador = new CD_ComparaPassword();
+        }
+        public bool ComparaPass(string username, string password)
+        {
+            string concatenados=password+username;
+            string passwordHasheado = Seguridad.SHA256(concatenados);
+            bool correcto = comparador.ComparaPassD(username, passwordHasheado);
+            if (correcto)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
