a71feac0c3bd518b9a9500c06aebdd194e96ff2f
FedeSistemas23 <fededelveliz2017@gmail.com>
2026-05-30 10:52:06 -0300

    agregue la clase persona

---
+++ Viajando (2)/Viajando/CapaSesion/CS_AtributosPersona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaSesion
+{
+    public class CS_AtributosPersona
+    {
+        public int IdPersona { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string DNI { get; set; }
+
+        public string Email { get; set; }
+
+        public bool Activo { get; set; }
+
+
+    }
+}
